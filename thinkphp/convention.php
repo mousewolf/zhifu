8f9eeb086319d3961b193284ae0c05f93206da2c
Your Name <you@example.com>
2019-03-10 14:18:28 +0800

    ss

--- thinkphp/convention.php
+++ thinkphp/convention.php
@@ -246,7 +246,7 @@ return [
         // 服务器地址
         'hostname'        => '127.0.0.1',
         // 数据库名
-        'database'        => '',
+        'database'        => 'www.zf.com',
         // 数据库用户名
         'username'        => 'root',
         // 数据库密码
@@ -258,7 +258,7 @@ return [
         // 数据库编码默认采用utf8
         'charset'         => 'utf8',
         // 数据库表前缀
-        'prefix'          => '',
+        'prefix'          => 'cm_',
         // 数据库调试模式
         'debug'           => false,
         // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
